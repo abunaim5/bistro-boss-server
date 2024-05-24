52bf6254da9ced7476a5c692f294f17fd385fd17
abunaim5 <abunaim140@gmail.com>
2024-05-25 01:00:17 +0600

    added some apis and verify access token

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -30,30 +31,99 @@ async function run() {
     const reviewCollection = client.db('bistroBossDB').collection('reviews');
     const cartCollection = client.db('bistroBossDB').collection('carts');
 
-    app.get('/menu', async(req, res) => {
+    app.get('/menu', async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
     });
 
-    app.get('/carts', async(req, res) => {
+    app.get('/carts', async (req, res) => {
       const email = req.query.email
-      const query = {email: email}
+      const query = { email: email }
       const result = await cartCollection.find(query).toArray();
       res.send(result);
     });
 
-    // users related api
-    app.post('/users', async(req, res) => {
+    // jwt related api
+    app.post('/jwt', async (req, res) => {
       const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token });
+    })
+
+    // middleware
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' })
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' });
+        }
+        req.decoded = decoded;
+        next();
+      })
+      console.log('Finder', req.headers.authorization)
+    }
+
+    // users related api
+    app.get('/users', verifyToken, async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existUser = await userCollection.findOne(query);
+
+      if (existUser) {
+        return res.send({ message: 'User already exist', insertedId: null });
+      }
+
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
 
-    app.post('/carts', async(req, res) => {
+    // Admin related apis
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+      const query = { email: email }
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin });
+    })
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        }
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.post('/carts', async (req, res) => {
       const data = req.body;
       const result = await cartCollection.insertOne(data);
       res.send(result);
-    })
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -67,9 +137,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Boss is Running');
+  res.send('Boss is Running');
 });
 
 app.listen(port, () => {
-    console.log(`Boss is Running on PORT ${port}`)
+  console.log(`Boss is Running on PORT ${port}`)
 })
